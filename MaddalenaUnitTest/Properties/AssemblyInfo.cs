fbaf4df3ff5192f3bb0363a3c6183ef85580d91b
Matteo Fabbri <matteo@phascode.org>
2017-09-21 17:53:07 +0200

    z

---
+++ MaddalenaUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MaddalenaUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaddalenaUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37da35da-6700-47f9-b9ff-b47fd401427e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MaddalenaUnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MaddalenaUnitTest.MaddalenaServiceReference;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaddalenaUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static string apikey;
+
+        public UnitTest1()
+        {
+            var apikeys = new ApiKeyServiceClient();
+            apikey = apikeys.Create("q@q.q");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var users = new UserServiceClient();
+            var u = users.ByEmail("q@q.q");
+            Assert.AreNotEqual(u, null);
+        }
+    }
+}
